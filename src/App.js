464c5b5b214d3884e272cbd4aabc14596a7bd410
nsunami <naoyuki.sunami@gmail.com>
2021-08-26 16:46:31 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [articleMetadata, setArticleMetadata] = useState("")
+  const [doi, setDoi] = useState("")
+  // const defaultDoi = "10.1037/0003-066X.59.1.29/"
+
+  // const fetchData = (doi = "10.1037/0003-066X.59.1.29/") => {
+  //   const doiURL = "https://api.crossref.org/works/" + doi
+  //   axios.get(doiURL)
+  //   .then(res => {
+  //       const newArticleMetadata = res.data.message
+  //       setArticleMetadata(newArticleMetadata)
+  //       console.log(articleMetadata)
+  //   })
+
+  useEffect(() => {
+    const doiURL = "https://api.crossref.org/works/" + doi
+    axios.get(doiURL)
+    .then(res => {
+        const newArticleMetadata = res.data.message
+        setArticleMetadata(newArticleMetadata)
+  })
+    return () => {
+      // cleanup
+    }
+  }, [doi])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <input type="text" 
+      name="doi"
+      onChange={e => setDoi(e.target.value)}
+       ></input>
+        <button
+         >Resolve [Not Working...]</button>
+      </div>
+      <div>
+        {/* Show the results */} Title:
+        <strong>{articleMetadata.title}</strong> 
+        <div></div>
+      </div>
+      <div>
+      For Testing: 10.1037/0003-066X.59.1.29/
+      </div>
     </div>
   );
 }
