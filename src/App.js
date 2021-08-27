c7b4eb35ef05cf942016055820e4cf1ce7592627
nsunami <naoyuki.sunami@gmail.com>
2021-08-26 21:30:07 -0400

    use async-await

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import './App.css';
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [articleMetadata, setArticleMetadata] = useState("")
-  const [doi, setDoi] = useState("")
-  // const defaultDoi = "10.1037/0003-066X.59.1.29/"
+  const defaultArticleMetadata = {
+    title: "UNDEFINED",
+  };
+  const defaultDoi = "10.1111/spc3.12497";
 
-  // const fetchData = (doi = "10.1037/0003-066X.59.1.29/") => {
-  //   const doiURL = "https://api.crossref.org/works/" + doi
-  //   axios.get(doiURL)
-  //   .then(res => {
-  //       const newArticleMetadata = res.data.message
-  //       setArticleMetadata(newArticleMetadata)
-  //       console.log(articleMetadata)
-  //   })
+  const [doi, setDoi] = useState(defaultDoi);
+  const [articleMetadata, setArticleMetadata] = useState(defaultArticleMetadata);
+
+async function getArticleMetadata() {
+    const doiURL = "https://api.crossref.org/works/" + doi;
+    const response = await axios.get(doiURL);
+    const message = response.data.message 
+    setArticleMetadata(message)
+  }
 
   useEffect(() => {
-    const doiURL = "https://api.crossref.org/works/" + doi
-    axios.get(doiURL)
-    .then(res => {
-        const newArticleMetadata = res.data.message
-        setArticleMetadata(newArticleMetadata)
-  })
-    return () => {
-      // cleanup
-    }
-  }, [doi])
+    getArticleMetadata()
+  }, [doi]);
+
 
   return (
     <div className="App">
       <div>
-      <input type="text" 
-      name="doi"
-      onChange={e => setDoi(e.target.value)}
-       ></input>
+        <input
+          type="text"
+          name="doi"
+          onChange={(e) => setDoi(e.target.value)}
+        ></input>
         <button
-         >Resolve [Not Working...]</button>
+         onClick={null}>
+         Resolve [Not Working...]
+         </button>
       </div>
       <div>
         {/* Show the results */} Title:
-        <strong>{articleMetadata.title}</strong> 
+        <strong>{articleMetadata.title}</strong>
         <div></div>
       </div>
-      <div>
-      For Testing: 10.1037/0003-066X.59.1.29/
-      </div>
+      <div>For Testing: 10.1037/0003-066X.59.1.29/</div>
     </div>
   );
 }
