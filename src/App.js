6a01b194a1c28315e68368693692aa02fafe0c64
nsunami <naoyuki.sunami@gmail.com>
2021-08-30 13:40:02 -0400

    style with tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import axios from "axios";
 import ArticleList from "./ArticleList";
+import { FaGithub } from "react-icons/fa";
 
 function App() {
   const defaultArticleMetadata = {
     title: "[Enter DOI to get the article's title]",
   };
-  const defaultDoi = "10.1111/spc3.12497";
+  const defaultDoi = "10.3390/publications7020040";
 
   const [doi, setDoi] = useState(defaultDoi);
   const [articleMetadata, setArticleMetadata] = useState(
@@ -27,27 +28,46 @@ function App() {
   function handleArticleNotFound() {
     setArticleMetadata({
       title: "[ERROR: Article Not Found]",
-    })
+    });
   }
 
   return (
-    <div className="App">
-      <nav>
-        <a href="https://github.com/nsunami/doi-resolver" rel="noreferrer" target="_blank">
-        GitHub Repo
-        </a>
-      </nav>
-
-      <div>
-        Enter DOI: <input
-          type="text"
-          name="doi"
-          value={doi}
-          onChange={(e) => setDoi(e.target.value)}
-        ></input>
-        <button onClick={getArticleMetadata}>Resolve</button>
+    <div>
+      <header className="bg-gray-50 p-6">
+        <nav className="">
+          <ul className="">
+            <li className="mr-6 inline hover:underline"><a href="/">DOI Resolver</a></li>
+            <li className="mr-6 inline hover:underline">
+              <a
+                href="https://github.com/nsunami/doi-resolver"
+                rel="noreferrer"
+                target="_blank"
+              >
+                <FaGithub className="inline" /> GitHub Repo
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="m-6">
+        <div className="m-1 p-6 bg-green-50 rounded-md">
+          <input
+            placeholder="Enter DOI"
+            className="border border-transparent focus:outline-none focus:ring-2 rounded-md shadow-md w-60"
+            type="text"
+            name="doi"
+            value={doi}
+            onChange={(e) => setDoi(e.target.value)}
+          ></input>
+          <button
+            className="bg-yellow-500 hover:bg-yellow-600 mx-2 px-2 border rounded-md"
+            onClick={getArticleMetadata}
+          >
+            Resolve
+          </button>
+        </div>
+        <ArticleList articleMetadata={articleMetadata} />
       </div>
-      <ArticleList articleMetadata={articleMetadata}/>
     </div>
   );
 }
--- src/Article.js
+++ src/Article.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { FaBook, FaUser } from 'react-icons/fa';
+
 
 export default function Article(props) {
   const { articleMetadata } = props;
   return (
-    <div className="border-2 border-indigo-600">
-      <div className="arcile__title">{articleMetadata?.title}</div>
-      <div className="article__author">
+    <div className="m-2 p-2 bg-yellow-50 rounded">
+      <div
+       id="author"
+       className="font-semibold">{articleMetadata?.title}</div>
+      <div className="article__author ml-2 text-gray-700">
+      <FaUser className="inline mr-2" /> 
         {articleMetadata.author.map(
             (author, i, authors) => {
                 if (authors.length - 1 === i) {
@@ -17,7 +22,14 @@ export default function Article(props) {
             }
         )}
       </div>
-      <div className="article__DOI colors.gray">{articleMetadata.DOI}</div>
+      <div className="article__DOI ml-2 text-gray-700">
+      <a 
+        href={articleMetadata?.link[0].URL}
+        rel="noreferrer" target="_blank">
+        <FaBook className="inline mr-2"/>
+        {articleMetadata.DOI}
+      </a>
+      </div>
     </div>
   );
 }
